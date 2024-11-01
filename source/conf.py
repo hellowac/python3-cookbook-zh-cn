017f32239c3d70d4023e489c34db16d74d29ef8b
hellowac <hellowxchao@gmail.com>
2024-11-01 14:07:39 +0800

    更新部署到github pages

--- source/conf.py
+++ source/conf.py
@@ -18,51 +18,53 @@ import os
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#sys.path.insert(0, os.path.abspath('.'))
+# sys.path.insert(0, os.path.abspath('.'))
 
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
-#needs_sphinx = '1.0'
+# needs_sphinx = '1.0'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['chinese_search']
+extensions = ["chinese_search"]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ["_templates"]
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = ".rst"
 
 # The encoding of source files.
-#source_encoding = 'utf-8-sig'
+# source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'index'
+master_doc = "index"
 
 # General information about the project.
-project = u'python3-cookbook'
-copyright = u'2017, 熊能'
+project = "python3-cookbook"
+copyright = "2017, 熊能"
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The short X.Y version.
-version = '3.0'
+version = "3.0"
 # The full version, including alpha/beta/rc tags.
-release = '3.0.0'
+release = "3.0.0"
 
 exclude_patterns = []
 
-html_theme = 'default'
+# html_theme = 'default'
+html_theme = "furo"
+language = "zh-CN"
 
-html_static_path = ['_static']
+html_static_path = ["_static"]
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'python3-cookbook doc'
+htmlhelp_basename = "python3-cookbook doc"
 
 
 # -- Options for LaTeX output ---------------------------------------------
@@ -70,13 +72,15 @@ htmlhelp_basename = 'python3-cookbook doc'
 # 注：在生成html的时候这句话要注释
 # latex_engine = 'xelatex'
 
-latex_elements={# The paper size ('letterpaper' or 'a4paper').
-'papersize':'a4paper',# The font size ('10pt', '11pt' or '12pt').
-'pointsize':'12pt','classoptions':',oneside','babel':'',#必须
-'inputenc':'',#必须
-'utf8extra':'',#必须
-# Additional stuff for the LaTeX preamble.
-'preamble': r"""
+latex_elements = {  # The paper size ('letterpaper' or 'a4paper').
+    "papersize": "a4paper",  # The font size ('10pt', '11pt' or '12pt').
+    "pointsize": "12pt",
+    "classoptions": ",oneside",
+    "babel": "",  # 必须
+    "inputenc": "",  # 必须
+    "utf8extra": "",  # 必须
+    # Additional stuff for the LaTeX preamble.
+    "preamble": r"""
 \usepackage{xeCJK}
 \usepackage{indentfirst}
 \setlength{\parindent}{2em}
@@ -86,27 +90,24 @@ latex_elements={# The paper size ('letterpaper' or 'a4paper').
 \setCJKfamilyfont{sf}{WenQuanYi Micro Hei}
 \XeTeXlinebreaklocale "zh"
 \XeTeXlinebreakskip = 0pt plus 1pt
-"""}
+""",
+}
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-  ('index', 'python3-cookbook.tex', u'《Python Cookbook》第三版',
-   u'熊能', 'howto'),
+    ("index", "python3-cookbook.tex", "《Python Cookbook》第三版", "熊能", "howto"),
 ]
 
 # -- Options for manual page output ---------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
-man_pages = [
-    ('index', 'python3-cookbook', u'《Python Cookbook》第三版',
-     [u'熊能'], 1)
-]
+man_pages = [("index", "python3-cookbook", "《Python Cookbook》第三版", ["熊能"], 1)]
 
 # If true, show URL addresses after external links.
-#man_show_urls = False
+# man_show_urls = False
 
 
 # -- Options for Texinfo output -------------------------------------------
@@ -115,22 +116,29 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'python3-cookbook', u'《Python Cookbook》第三版',
-   u'熊能', 'python3-cookbook', '《Python Cookbook》第三版',
-   'Miscellaneous'),
+    (
+        "index",
+        "python3-cookbook",
+        "《Python Cookbook》第三版",
+        "熊能",
+        "python3-cookbook",
+        "《Python Cookbook》第三版",
+        "Miscellaneous",
+    ),
 ]
 
 
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # on_rtd is whether we are on readthedocs.org, this line of code grabbed from docs.readthedocs.org
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+# on_rtd = os.environ.get("READTHEDOCS", None) == "True"
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+# if not on_rtd:  # only import and set the theme if we're building docs locally
+#     import sphinx_rtd_theme
+
+#     html_theme = "sphinx_rtd_theme"
+#     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 # otherwise, readthedocs.org uses their theme by default, so no need to specify it
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 _exts = "../exts"
 sys.path.append(os.path.abspath(_exts))
